664074ecdc19c7eae260fd9271bbe39cde3ac39e
lankai2000 <156618579+lankai2000@users.noreply.github.com>
2024-07-04 19:48:43 +0300

    Add files via upload

--- Part_1/blok_1/Task4.sql
+++ Part_1/blok_1/Task4.sql
@@ -1,1 +1,20 @@
+INSERT INTO Pacient (id_cart, Vid, age, nickname, id_doc, diagnoz)
+    VALUES
+        (1, 'cat', 3, 'barsik', 4, 'lishai'),
+        (2, 'dog', 5, 'Rex', 2, 'greep'),
+        (3, 'horse', 2, 'chaika', 3, 'ratrue'),
+        (4, 'parrot', 13, 'Kesha', 1, 'ornitoz')
 
+INSERT INTO Doktor ( id_doc, ferstname, secondname, lastname, speciality)
+    VALUES
+        (1, 'Ivan', 'Ivanovich', 'Ivanov', 'birds'),
+        (2, 'Petr', 'Petrovich', 'Petrov', 'small_animals'),
+        (3, 'Jon', 'Ibodovich', 'Ivanov', 'big_animas'),
+        (4, 'Olga', 'Kirilovna', 'Grabina', 'cat')
+
+INSERT INTO  diagnoz (diagnoz, mode, medicine, doza, surgery,)
+    VALUES
+         ( 'lishai', 'irradiasion', 'ambiol', 5, FALSE)
+         ( 'greep', 'injections', 'fokorol', 2, FALSE)
+         ( 'ratrue', 'injections', 'rokotan', 20, TRUE)
+         ( 'ornitoz', 'pills', 'aspirin', 1, FALSE)
